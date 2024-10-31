1a8680feebc2660aaba9a8cb25f693a85fd1aadc
Filip <fb1112@nyu.edu>
2024-10-31 21:06:05 +0800

    minor comment edit on q1

--- src/main.zig
+++ src/main.zig
@@ -1,7 +1,7 @@
 const std = @import("std");
 const print = std.debug.print;
-const q1 = @import("q1.zig");
+const q2 = @import("q2.zig");
 pub fn main() !void {
-    const q1_answer = try q1.q1();
+    const q1_answer = try q2.q2();
     print("{any}\n", .{q1_answer});
 }
